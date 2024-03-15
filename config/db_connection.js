7faba96a8155d4f4d491f1d5d2439652a7c77548
Sabari B <111430395+Sabari711@users.noreply.github.com>
2024-03-15 09:54:43 +0530

    Update db_connection.js to set cluster url

--- config/db_connection.js
+++ config/db_connection.js
@@ -2,8 +2,11 @@ const Mongoose = require('mongoose');
 const config = require("./config")
    
 const mongoDbUrl = {
-    DB_HOST: `${config.HOST_URL}`
+    DB_HOST: `mongodb+srv://balasabari27:TkBtUjS5fonMPrIP@democluster.kj0hmur.mongodb.net/demo`
 }
+// const mongoDbUrl = {
+//     DB_HOST: `${config.HOST_URL}`
+// }
 
 Mongoose
 .connect(mongoDbUrl.DB_HOST, { useNewUrlParser: true,  useUnifiedTopology: true  })
